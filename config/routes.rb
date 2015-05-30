8938f3a62ee17a02245424d83591fd6c657efc3e
Richard Greene <greener13@students.ecu.edu>
2015-05-30 14:30:54 -0400

    Changes to Homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'page/home'
+  root "pages#home"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
